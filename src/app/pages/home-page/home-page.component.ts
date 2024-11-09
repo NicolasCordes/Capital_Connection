7d3d5a28d8939e97e9af1b7981c4a557a5977b44
Santiago <santiagofidelibus19@gmail.com>
2024-11-09 19:38:49 -0300

    home page updated

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../auth/services/service.service';
-import { EntrepreneurshipFormComponent } from '../../features/entrepreneurship/components/entrepreneurship-form-component/entrepreneurship-form-component.component';
+import { EntrepreneurshipService } from '../../features/entrepreneurship/services/entrepreneurship.service';
+import { Entrepreneurship } from '../../features/entrepreneurship/models/entrepreneurship.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css',
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit, OnDestroy {
   username = 'invitado';
   private sub?: Subscription;
+  entrepreneurships: Entrepreneurship[] = [];
+  currentIndex = 0; // Indice actual del slider
+  carouselTransform = 'translateX(0)'; // Control del desplazamiento del slider
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private entrepreneurshipService: EntrepreneurshipService
+  ) {}
 
   ngOnInit(): void {
+    // Obtener entrepreneurships de la API
+    this.entrepreneurshipService.getEntrepreneurship(0, 9).subscribe({
+      next: (response) => {
+        this.entrepreneurships = response.content;
+      },
+      error: (err) => {
+        console.error('Error al obtener entrepreneurships', err);
+      },
+    });
+
+    // Suscripción al servicio de autenticación
     this.sub = this.authService.auth().subscribe({
       next: (activeUser) => {
         if (activeUser) {
@@ -30,5 +50,18 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
+  }
+
+  moveSlider(direction: 'left' | 'right'): void {
+    const totalSlides = this.entrepreneurships.length;
+    const slidesVisible = 3; // Cambiado de 5 a 3
+    if (direction === 'left') {
+      this.currentIndex = (this.currentIndex - slidesVisible + totalSlides) % totalSlides;
+    } else {
+      this.currentIndex = (this.currentIndex + slidesVisible) % totalSlides;
+    }
+
+    // Actualiza el transform del contenedor del slider para mover las tarjetas
+    this.carouselTransform = `translateX(-${this.currentIndex * (100 / slidesVisible)}%)`;
   }
 }
