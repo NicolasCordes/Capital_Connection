65044e2f0b599306b87cd6d5c6cb940e6e3188be
NicolasCordes <nicolascordes1@gmail.com>
2025-02-12 20:10:00 -0300

    actualizar las URLs del entorno para apuntar a los servicios en producción

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,22 +1,22 @@
 export const environment = {
-  urlBase: 'https://crowdnet-api-2-0.onrender.com/',
+  urlBase: 'https://crowdnet-api-2-0.onrender.com',
   API_KEY: '844878334141893',
-  urlTBE: 'http://localhost:3000',
+  urlTBE: 'https://mercadopago-backend.onrender.com',
   API_SECRET: 'ybMtgjOgrDmOuD0-h5Cgg3kSO-w',
   CLOUD_NAME: 'dyho1ydzl',
-  urlServer: 'https://crowdnet-api-2-0.onrender.com/',
+  urlServer: 'https://crowdnet-api-2-0.onrender.com',
   scriptMercadoPago: 'https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js',
-  urlFr: 'http://localhost:4200',
+  urlFr: 'https://capital-connection.onrender.com',
   googleOAuth: {
     clientId: '990693061222-lbdeiil1n280tp0d9udkcbr6n1fs9m3i.apps.googleusercontent.com',
-    redirectUri: 'http://localhost:4200/callback',
+    redirectUri: 'https://capital-connection.onrender.com/callback',
     authUrl: 'https://accounts.google.com/o/oauth2/v2/auth'
   }
 };
 export const environmentMP = {
   publicKeyMP:'TEST-5111c7d5-22a9-49ce-801a-7b8a961df5cf',
   token: 'TEST-6083462466960165-101910-ab0d02d54a828750e570c73c7ce1d464-581495007',
-  ngrokURL:'https://e3af-200-105-34-95.ngrok-free.app'
+  ngrokURL:'https://mercadopago-backend.onrender.com/webhook'
 }
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  urlBase: 'https://crowdnet-api-2-0.onrender.com/',
+  urlBase: 'https://crowdnet-api-2-0.onrender.com',
   API_KEY: '844878334141893',
-  urlTBE: 'http://localhost:3000',
+  urlTBE: 'https://mercadopago-backend.onrender.com',
   API_SECRET: 'ybMtgjOgrDmOuD0-h5Cgg3kSO-w',
   CLOUD_NAME: 'dyho1ydzl',
-  urlServer: 'https://crowdnet-api-2-0.onrender.com/',
+  urlServer: 'https://crowdnet-api-2-0.onrender.com',
   scriptMercadoPago: 'https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js',
   googleOAuth: {
     clientId: '990693061222-lbdeiil1n280tp0d9udkcbr6n1fs9m3i.apps.googleusercontent.com',
-    redirectUri: 'http://localhost:4200/callback',
+    redirectUri: 'https://capital-connection.onrender.com/callback',
     authUrl: 'https://accounts.google.com/o/oauth2/v2/auth'
   }
 };
