52a6279f518a12a81165e1d104e1c83841f604b4
Santiago <santiagofidelibus19@gmail.com>
2025-02-12 21:05:00 -0300

    cloudinary fixes

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,6 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  
+    if (req.url.includes('cloudinary.com')) {
+      return next(req);
+    }
+
   const accessToken = localStorage.getItem("access_token");
 
   if (accessToken) {
--- src/app/services/mp.service.ts
+++ src/app/services/mp.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environmentMP } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class MpService {
   constructor(private http: HttpClient) {}
 
   crearPreferencia(descripcion: string, cantidad: number, precio: number, iddon: number, idacc: number): Observable<any> {
-    return this.http.post<any>('http://localhost:3000/create_preference', {
+    return this.http.post<any>(`${environment.urlTBE}/create_preference`, {
       title: descripcion,
       quantity: cantidad,
       price: precio,
@@ -19,7 +21,4 @@ export class MpService {
       idacc: idacc
     });
   }
-
-
-
 }
